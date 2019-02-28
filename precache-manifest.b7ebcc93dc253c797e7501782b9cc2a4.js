5eb537601f984499f179d81b32638f87e32cc1be
tammytangg <tammytangg@gmail.com>
2019-02-28 22:04:49 +0900

    Updates

---
+++ precache-manifest.b7ebcc93dc253c797e7501782b9cc2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae24a4ee86906c1a589c",
+    "url": "/react-blog-demo/static/js/runtime~main.ae24a4ee.js"
+  },
+  {
+    "revision": "ad44161a1e5cf9c0bf68",
+    "url": "/react-blog-demo/static/js/main.ad44161a.chunk.js"
+  },
+  {
+    "revision": "7fee77e165fdfc110c74",
+    "url": "/react-blog-demo/static/js/2.7fee77e1.chunk.js"
+  },
+  {
+    "revision": "ad44161a1e5cf9c0bf68",
+    "url": "/react-blog-demo/static/css/main.868210e0.chunk.css"
+  },
+  {
+    "revision": "3d2edfddd3b34040021eb5c4bd122d5b",
+    "url": "/react-blog-demo/index.html"
+  }
+];
